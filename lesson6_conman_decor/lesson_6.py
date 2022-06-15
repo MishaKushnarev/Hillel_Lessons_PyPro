62f3c41e61f8dc6b891dc6b3957ec089ee9ef2d8
misha <deadly.algorythm@gmail.com>
2022-06-15 17:30:09 +0300

    Fixed

--- lesson6_conman_decor/lesson_6.py
+++ lesson6_conman_decor/lesson_6.py
@@ -1,9 +1,11 @@
 import functools
 import os
 
+
 # MODIFY THIS DECORATOR
 def reverse_string(func):
     """If output is a string, reverse it. Otherwise, return None."""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         data = func()
@@ -11,30 +13,32 @@ def reverse_string(func):
             result_string = data[::-1]
         else:
             pass
+
     return wrapper
+
 
 # TARGET FUNCTIONS
 @reverse_string
 def get_university_name() -> str:
     return "Western Institute of Technology and Higher Education"
 
+
 @reverse_string
 def get_university_founding_year() -> int:
     return 1957
 
+
 # TEST OUPUT
-os.system('cls||clear')
-print(
-    get_university_name(),
-    get_university_founding_year(),
-    sep="\n"
-)
+os.system("cls||clear")
+print(get_university_name(), get_university_founding_year(), sep="\n")
+
 
 # MODIFY THIS DECORATOR
 def mask_data(target_key: str, replace_with: str = "*"):
     """Replace the value of a dictionary with a 'masked' version."""
+
     def decorator(func):
-        @functools.wraps(func)    
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
             tmp_list = []
             data = func(*args, **kwargs)
@@ -43,26 +47,23 @@ def mask_data(target_key: str, replace_with: str = "*"):
                 tmp_list.append(name)
 
                 for i in range(len(tmp_list)):
-                    tmp_list[i]="*"
-                    result_list = "".join(tmp_list) 
+                    tmp_list[i] = "*"
+                    result_list = "".join(tmp_list)
                     data["name"] = result_list
 
             return data
+
         return wrapper
+
     return decorator
+
 
 # TARGET FUNCTIONS
 @mask_data(target_key="name")
 def get_user(name: str, age: int):
-    return {
-        "name": name,
-        "age": age
-    }
+    return {"name": name, "age": age}
+
 
 # TEST OUTPUT
-os.system('cls||clear')
-print(
-    get_user(name="Alice", age=30),
-    get_user(name="Bob", age=25),
-    sep="\n"
-)
+os.system("cls||clear")
+print(get_user(name="Alice", age=30), get_user(name="Bob", age=25), sep="\n")

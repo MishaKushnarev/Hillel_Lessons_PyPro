1c095be730a20539f8f619296bff5b78aea65518
DeadlyAlgorythm <deadly.algorythm@gmail.com>
2022-07-15 12:38:27 +0300

    upd

--- lesson13_API/lesson13.py
+++ lesson13_API/lesson13.py
@@ -1,35 +1,3 @@
-# from urllib import response
-# import httpx
-# import asyncio
-# import random
-# import aiohttp
-
-# MAX_POKEMON = 100
-# SIZE = 50
-# BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
-
-# def get_random_id() -> str:
-#     random_id = random.randint(1, MAX_POKEMON + 1)
-#     return str(random_id)
-
-# async def get_pokemon_async(id_: str) -> str:
-#     url = BASE_URL + id_
-#     async with aiohttp.ClientSession() as session:
-#         response = await session.get(url)
-#         return response.json()["name"]
-
-# async def get_random_pokemon_async() -> str:
-#     random_id = get_random_id()
-#     return await get_pokemon_async(random_id)
-
-# async def async_main():
-#     tasks = [get_random_pokemon_async() for _ in range(SIZE)]
-#     random_pokemons = await asyncio.gather(*tasks)
-#     print(f"Pokemons: {random_pokemons}")
-
-# asyncio.run(async_main())
-
-        
 import asyncio
 import random
 import aiohttp
@@ -37,6 +5,7 @@ import aiohttp
 MAX_POKEMON = 100
 SIZE = 1
 BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
+
 
 def get_random_id() -> str:
     random_id = random.randint(1, MAX_POKEMON + 1)

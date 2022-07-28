feaf1e236b2b93b1e7814138db942ea98d813f87
DeadlyAlgorythm <deadly.algorythm@gmail.com>
2022-07-28 17:25:38 +0300

    done

--- lesson_11_async_threads/lesson11.py
+++ lesson_11_async_threads/lesson11.py
@@ -1,75 +1,77 @@
-# import asyncio
-# import random
-# from threading import Thread
-# from typing import List
+import asyncio
+import random
+from threading import Thread
+from typing import List
 
-# # RND_MIN_VALUE = 1
-# # COUNT_ELEMENTS = 1000000
-
-# # def random_element(min_value1: int, max_value2: int) -> int:
-# #     return random.randint(min_value1, max_value2)
+RND_MIN_VALUE = 1
+COUNT_ELEMENTS = 1000000
+data = []
 
 
-# # def T1(count: int) -> List:
-# #     result_list = []
-
-# #     for _ in range(0, count):
-# #         result_list.append(random_element(RND_MIN_VALUE, COUNT_ELEMENTS))
-
-# #     return result_list
+def random_element(min_value1: int, max_value2: int) -> int:
+    return random.randint(min_value1, max_value2)
 
 
-# # def T2(data: list) -> int:
-# #     summ = 0
+def T1(data: list, count: int) -> List:
 
-# #     for element in data:
-# #         summ = summ + element
-# #     print(f"Summ is : {summ}")
-# #     return summ
+    for _ in range(0, count):
+        data.append(random_element(RND_MIN_VALUE, COUNT_ELEMENTS))
 
-
-# # def T3(count: int, summ: int) -> int:
-# #     result = round(count / summ)
-
-# #     return print("Average is: ", result)
+    return data
 
 
-# # def main():
-# #     data = Thread(target=T1, args=(COUNT_ELEMENTS, ))
-# #     summ = Thread(target=T2, args=(T1(COUNT_ELEMENTS), ))
-# #     average = Thread(target=T3, args=(T2(T1(COUNT_ELEMENTS)), COUNT_ELEMENTS))
+def T2(data: list) -> int:
+    summ = 0
 
-# #     data.start()
-# #     data.join()
-# #     summ.start()
-# #     average.start()
-
-# # if __name__ == '__main__':
-# #     main()
+    for element in data:
+        summ = summ + element
+    print(f"Summ is : {summ}")
+    return summ
 
 
-# def get_primes_amount(num: int) -> int:
-#     Results = 0
+def T3(data: list) -> int:
+    result = round(sum(data) / len(data))
 
-#     for i in num:
-#         counter = 0
-#         for j in range(1, i):
-#             if i % j == 0:
-#                 counter += 1
-#             if counter > 2:
-#                 break
-#         results += 1
-
-#     return results
+    return print("Average is: ", result)
 
 
-# numbers = [40000, 400, 1000000, 700]
+def main():
+    gen_data = Thread(target=T1, args=(data, COUNT_ELEMENTS))
+    summ = Thread(target=T2, args=(data,))
+    average = Thread(target=T3, args=(data,))
+
+    gen_data.start()
+    gen_data.join()
+    summ.start()
+    average.start()
 
 
-# def main():
-#     for i in numbers:
-#         get_primes_amount(i)
+if __name__ == "__main__":
+    main()
 
 
-# if __name__ == "__main__":
-#     main()
+async def get_primes_amount(num: int):
+    result = 0
+    counter = []
+    for i in range(2, num + 1):
+        if i % 2 != 0 and i % 3 != 0 and i % 5 != 0 and i % 7 != 0:
+            counter = [i for j in range(11, i + 1) if i % j == 0]
+
+            if len(counter) < 2:
+                result += 1
+                await asyncio.sleep(0)
+
+    print(result)
+
+
+def main(data: list) -> None:
+
+    task = [get_primes_amount(i) for i in data]
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(asyncio.gather(*task))
+    event_loop.close()
+
+
+if __name__ == "__main__":
+    numbers = [40000, 400, 1000000, 700]
+    main(numbers)
